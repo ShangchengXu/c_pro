31ce2f9ae35103883af5717b754cc8f793262a8e
Shangcheng Xu <fengior@163.com>
2020-09-02 19:47:33 +0800

    1

---
+++ test/test_5/try.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void swap (int & a, int & b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+// Using main to test
+int main(){
+    void swap(int & a, int  & b);
+
+    int a = 1, b = 2;
+    swap(a,b);
+    std::cout<< a << b ;
+    return 0;
+}
