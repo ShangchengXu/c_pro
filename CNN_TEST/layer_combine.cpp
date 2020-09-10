b6f51df86f16c9c92dbd5e15bb5e9644feea6933
boyce <fengior@163.com>
2020-09-10 22:29:02 +0800

    1

---
+++ CNN_TEST/layer_combine.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "layer.h"
+#include "function.h"
+#include <cmath>
+
+void Layer_combine::initialize()
+{   
+
+    function_new_double(frame_size);
+}
+
+
+
+
+
+Layer_combine::Layer_combine(int index_,int frame_num_, int frame_size_,int weight_size_)
+{
+
+}
--- CNN_TEST/main.cpp
+++ CNN_TEST/main.cpp
@@ -11,7 +11,28 @@ int main()
 {
     // max_pooling_test();
     // layer_maxpooling_test();
-    layer_cnn_test();
+    // layer_cnn_test();
+    // int *** p_p_p;
+    // int **p_p;
+    // p_p_p = new int **[1];
+    // p_p = new int *[2];
+    // int a = 0;
+    // for(int i = 0 ; i < 2 ; i++)
+    // {
+    //     p_p[i] = new int [2];
+    // }
+    // for(int i = 0 ; i < 2; i++)
+    // {
+    //     for(int j = 0 ; j < 2; j++)
+    //     {
+    //         p_p[i][j] = a;
+    //         a++;
+    //     }
+    // }
+    // p_p_p[0] = p_p;
+
+    // std::cout<<p_p_p[0][1][1];
+
 
    
 
