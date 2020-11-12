dfcbbf4e48137b5ba5ab3764f70d2a3166c16d3c
Shangcheng Xu <fengior@163.com>
2020-11-12 15:57:44 +0800

    1

--- imooc/cplusplus/2_2.cpp
+++ imooc/cplusplus/2_2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Student.h"
+#include "SortTestHelper.h"
 using namespace std;
 template<typename T>
 void selectionSort(T arr[],int n) {
@@ -12,14 +13,27 @@ void selectionSort(T arr[],int n) {
         swap(arr[i],arr[minIndex]);
     }
 }
+// int main() {
+//     int a[10] = {10,9,8,7,6,5,4,3,2,1};
+//     float b[10] = {2.5,2.4,4,5.5,3.4,2.9,10.3,9.9,8.7,1.1};
+//     Student d[4] = {{"D",90},{"C",100},{"B",95},{"A",95}};
+//     string c[4] = {"B","v","H","O"};
+//     selectionSort(d,4);
+//     for(int i = 0; i < 4; i++) {
+//         cout << d[i] <<" ";
+//     }
+//     return 0;
+// }
 int main() {
-    int a[10] = {10,9,8,7,6,5,4,3,2,1};
-    float b[10] = {2.5,2.4,4,5.5,3.4,2.9,10.3,9.9,8.7,1.1};
-    Student d[4] = {{"D",90},{"C",100},{"B",95},{"A",95}};
-    string c[4] = {"B","v","H","O"};
-    selectionSort(d,4);
-    for(int i = 0; i < 4; i++) {
-        cout << d[i] <<" ";
-    }
-    return 0;
+    int n = 100000;
+    int *arr = SortTestHelper::generateRandomArray(n,0,n);
+    // selectionSort(arr,10);
+    // for(int i = 0; i < 10; i++) {
+    // SortTestHelper::printArray(arr,10);
+    //     cout << arr[i] <<" ";
+    // }
+    SortTestHelper::testSort("Selection Sort", selectionSort, arr, n);
+
+delete [] arr;
+return 0;
 }
